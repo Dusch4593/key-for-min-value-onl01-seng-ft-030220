8b171352999a2bd614196c4926d51ab254ed8101
Brandon Dusch <brandondusch@gmail.com>
2020-03-15 13:28:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,17 @@
 
 require 'pry'
 
-def key_for_min_value(hash)
-  lowest_key = nil
-  lowest_value = nil
-  hash.each do |k, v|
-    if lowest_value == nil || v < lowest_value
-      lowest_value = v
-      lowest_key = k
-    end
+def key_for_min_value(name_hash)
+  if name_hash.empty?
+    return nil
   end
-  lowest_key
+  return_name = ""
+  lowest_value = name_hash.first[1]
+  name_hash.each_key{|key|
+    if(name_hash[key] <= lowest_value)
+      return_name = key
+      lowest_value = name_hash[key]
+    end
+  }
+  return_name
 end
